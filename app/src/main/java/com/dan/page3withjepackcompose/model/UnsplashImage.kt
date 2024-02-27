6bcd7cc121853f458de974c6872b4b3845d425d9
kim00030 <ilhyungkim@hotmail.com>
2024-02-27 18:01:46 -0500

    added @PrimaryKey annotation on id for Room

--- app/src/main/java/com/dan/page3withjepackcompose/model/UnsplashImage.kt
+++ app/src/main/java/com/dan/page3withjepackcompose/model/UnsplashImage.kt
@@ -2,6 +2,7 @@ package com.dan.page3withjepackcompose.model
 
 import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.dan.page3withjepackcompose.util.Constants
 
 /*
@@ -9,6 +10,8 @@ This model class will also represent a table in Room DB
  */
 @Entity(tableName = Constants.UNSPLASH_IMAGE_TABLE)
 data class UnsplashImage(
+    //RoomDB needs primary key, so I use id from server as Primary Key
+    @PrimaryKey(autoGenerate = false)
     val id: String,
     @Embedded
     val urls: Urls,
