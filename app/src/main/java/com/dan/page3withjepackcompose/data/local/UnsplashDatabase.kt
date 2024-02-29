9f4b3219dccf78954a189f6d44d91d1c383403f3
kim00030 <ilhyungkim@hotmail.com>
2024-02-29 09:09:24 -0500

    resolved dagger-hilt's version conflict in building process

--- app/src/main/java/com/dan/page3withjepackcompose/data/local/UnsplashDatabase.kt
+++ app/src/main/java/com/dan/page3withjepackcompose/data/local/UnsplashDatabase.kt
@@ -7,7 +7,7 @@ import com.dan.page3withjepackcompose.data.local.dao.UnsplashRemoteKeysDao
 import com.dan.page3withjepackcompose.model.UnsplashImage
 import com.dan.page3withjepackcompose.model.UnsplashRemoteKeys
 
-@Database(entities = [UnsplashImage::class, UnsplashRemoteKeys::class], version = 1)
+@Database(entities = [UnsplashImage::class, UnsplashRemoteKeys::class], version = 1, exportSchema = false)
 abstract class UnsplashDatabase : RoomDatabase() {
 
     abstract fun unsplashImageDao(): UnsplashImageDao
